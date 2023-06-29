e4687e47e830ba14be14469ab4a682079d07d592
Alaaeddine <dhifallah.alaaeddine@gmail.com>
2023-06-29 17:56:07 +0200

    HomeView : charger les villes et afficher-les

--- WeatherApp_TestPSA/ViewControllers/HomeViewController.swift
+++ WeatherApp_TestPSA/ViewControllers/HomeViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    //Outlets
+    @IBOutlet weak var citiesTableView: UITableView!
+    
+    //Properties
+    private var cities = [City]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
@@ -17,6 +23,15 @@ class HomeViewController: UIViewController {
             style: .plain,
             target: self,
             action: #selector(presentAddCityView))
+        
+        setupCitiesTableView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        cities = CoreDataManager.shared.fetchCities()
+        print(cities.count)
+        citiesTableView.reloadData()
     }
     
     @objc func presentAddCityView(){
@@ -28,5 +43,38 @@ class HomeViewController: UIViewController {
         }
         
         navigationController?.pushViewController(addCityVC, animated: true)
+    }
+}
+
+//MARK: TableViewDelegate
+extension HomeViewController: UITableViewDelegate{
+    
+    //Configurer le TableView des villes
+    private func setupCitiesTableView(){
+        citiesTableView.delegate = self
+        citiesTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        citiesTableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+//MARK: TableViewDataSource
+extension HomeViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let city = cities[indexPath.row]
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = city.name
+        cell.detailTextLabel?.text = city.country != nil ? city.country! : "--"
+        
+        return cell
     }
 }

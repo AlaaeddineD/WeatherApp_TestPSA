9fc650b7944d934dad8f4cdc015a47d4e07bab3f
Alaaeddine <dhifallah.alaaeddine@gmail.com>
2023-06-29 15:37:43 +0200

    AddCityView: configurer la SearchBar

--- WeatherApp_TestPSA/ViewControllers/AddCityViewController.swift
+++ WeatherApp_TestPSA/ViewControllers/AddCityViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 class AddCityViewController: UIViewController {
     
     static let storyboardIdentifier = "AddCityView"
-
+    
+    //MARK: Properties
+    private var searchController: UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Add City"
+        
+        setupSearchBar()
+    }
+}
+
+//MARK: Search bar
+extension AddCityViewController: UISearchResultsUpdating{
+    
+    //Configurer le SearchBar
+    private func setupSearchBar(){
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+    }
+    
+    //Recevoir des mises à jour lorsque le texte de la barre de recherche est modifié
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchText = searchController.searchBar.text!
     }
 }

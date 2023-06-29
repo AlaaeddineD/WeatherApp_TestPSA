8e20260ebdb785bfdd5e6105c6f1adae18452391
Alaaeddine <dhifallah.alaaeddine@gmail.com>
2023-06-29 16:04:55 +0200

    Rechercher des villes à l'aide de MK
    AddCityViewController: Recherchez des villes à l'aide de MK et affichez-les dans un TableView

--- WeatherApp_TestPSA/ViewControllers/AddCityViewController.swift
+++ WeatherApp_TestPSA/ViewControllers/AddCityViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
 class AddCityViewController: UIViewController {
     
     static let storyboardIdentifier = "AddCityView"
     
+    //Outlets
+    @IBOutlet weak var suggestionsTableView: UITableView!
+    
     //MARK: Properties
     private var searchController: UISearchController!
+    private var searchCompleter = MKLocalSearchCompleter()
+    private var searchResults = [MKLocalSearchCompletion]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,8 @@ class AddCityViewController: UIViewController {
         title = "Add City"
         
         setupSearchBar()
+        setupSearchCompleter()
+        setupSuggestionsTableView()
     }
 }
 
@@ -34,11 +43,71 @@ extension AddCityViewController: UISearchResultsUpdating{
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
         navigationItem.hidesSearchBarWhenScrolling = false
-        
     }
     
     //Recevoir des mises à jour lorsque le texte de la barre de recherche est modifié
     func updateSearchResults(for searchController: UISearchController) {
         let searchText = searchController.searchBar.text!
+        if searchText.count > 0 {
+            searchCompleter.queryFragment = searchText
+        }else {
+            searchResults.removeAll()
+            suggestionsTableView.reloadData()
+        }
+    }
+}
+
+//MARK: SearchCompleter
+extension AddCityViewController: MKLocalSearchCompleterDelegate{
+    
+    //Configurer le SearchCompleter
+    private func setupSearchCompleter(){
+        searchCompleter.delegate = self
+        searchCompleter.region = MKCoordinateRegion(.world)
+        searchCompleter.pointOfInterestFilter = .excludingAll
+    }
+    
+    //Appelé chaque fois que le searchCompleter a de nouveaux résultats
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        suggestionsTableView.reloadData()
+    }
+    
+    //Appelé lorsqu'il y avait une erreur avec le searchCompleter
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Search completer a rencontré une erreur: \(error.localizedDescription)")
+    }
+}
+
+//MARK: TableViewDataSource
+extension AddCityViewController: UITableViewDataSource{
+    
+    //Configurer le TableView des suggestions
+    private func setupSuggestionsTableView(){
+        suggestionsTableView.delegate = self
+        suggestionsTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let searchResult = searchResults[indexPath.row]
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.subtitle
+        
+        return cell
+    }
+}
+
+//MARK: TableViewDelegate
+extension AddCityViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        suggestionsTableView.deselectRow(at: indexPath, animated: true)
     }
 }
